092fe9166f2f864274d0bd2b7849e5e547108a41
ajay.meena <ajay.meena@flipkart.com>
2017-09-26 22:42:15 +0530

    added guice bundle

--- src/main/java/dropwizard/app/RestApplication.java
+++ src/main/java/dropwizard/app/RestApplication.java
@@ -1,21 +1,25 @@
 package dropwizard.app;
 
 
+import com.google.inject.Guice;
+import com.hubspot.dropwizard.guice.GuiceBundle;
 import dropwizard.app.config.RestAppConfiguration;
 import dropwizard.app.dynamic_feature.AuthDynamicFeature;
 import dropwizard.app.filter.AppFilter;
 import dropwizard.app.healthcheck.RestAppHealthCheck;
 import dropwizard.app.manged.RestAppManaged;
+import dropwizard.app.module.RestAppModule;
 import dropwizard.app.resource.TestResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.eclipse.jetty.server.Dispatcher;
+
 
 import javax.servlet.DispatcherType;
 import java.util.EnumSet;
 
 public class RestApplication extends Application<RestAppConfiguration> {
+    private GuiceBundle<RestAppConfiguration> guiceBundle;
 
     @Override
     public void run(RestAppConfiguration configuration, Environment environment) throws Exception {
@@ -28,7 +32,11 @@ public class RestApplication extends Application<RestAppConfiguration> {
 
     @Override
     public void initialize(Bootstrap<RestAppConfiguration> bootstrap){
-        //configuration related things goes here
+        guiceBundle = GuiceBundle.<RestAppConfiguration>newBuilder()
+                .addModule(new RestAppModule())
+                .setConfigClass(RestAppConfiguration.class)
+                .build();
+        bootstrap.addBundle(guiceBundle);
     }
 
     public static void main(String [] args) throws Exception{
---
+++ src/main/java/dropwizard/app/module/RestAppModule.java
@@ -0,0 +1,10 @@
+package dropwizard.app.module;
+
+import com.google.inject.AbstractModule;
+
+public class RestAppModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
